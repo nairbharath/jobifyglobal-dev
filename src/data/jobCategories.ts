9380e3ec2ce2e42cf47110459e19529717dbfdf2
Bharath Nair <nairbharath007@gmail.com>
2025-11-22 04:12:14 +0530

    manual changes in job categories

--- src/data/jobCategories.ts
+++ src/data/jobCategories.ts
@@ -1,10 +1,25 @@
+// export const jobCategories = [
+//   { label: "Construction Worker", value: "Construction Worker" },
+//   { label: "Electrician", value: "Electrician" },
+//   { label: "Plumber", value: "Plumber" },
+//   { label: "Welder", value: "Welder" },
+//   { label: "Fabricator", value: "Fabricator" },
+//   { label: "Driver", value: "Driver" },
+//   { label: "Hospitality Staff", value: "Hospitality Staff" },
+//   { label: "Cleaning Staff", value: "Cleaning Staff" },
+// ];
+
+
 export const jobCategories = [
-  { label: "Construction Worker", value: "Construction Worker" },
-  { label: "Electrician", value: "Electrician" },
-  { label: "Plumber", value: "Plumber" },
   { label: "Welder", value: "Welder" },
-  { label: "Fabricator", value: "Fabricator" },
-  { label: "Driver", value: "Driver" },
+  { label: "Welding Foreman", value: "Welding Foreman" },
+  { label: "Airless Spray Painter", value: "Airless Spray Painter" },
+  { label: "Structural Fabricator", value: "Structural Fabricator" },
+  { label: "Sand Blaster", value: "Sand Blaster" },
+  { label: "CNC Milling Programmer With Operator", value: "CNC Milling Programmer With Operator" },
+  { label: "CNC Lathe Operator With Programmer", value: "CNC Lathe Operator With Programmer" },
+  { label: "Painter", value: "Painter" },
   { label: "Hospitality Staff", value: "Hospitality Staff" },
   { label: "Cleaning Staff", value: "Cleaning Staff" },
+  { label: "Driver", value: "Driver" },
 ];
